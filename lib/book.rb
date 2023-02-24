19b694b65ee3b5eabb7b77bfe9f7d30e0219473e
Boston Lowrey <bostonlowrey@gmail.com>
2023-02-24 11:21:16 -0500

    feat: Book initializes with attributes

--- lib/book.rb
+++ lib/book.rb
@@ -1,3 +1,13 @@
 class Book
   
+  attr_reader :title, 
+              :author,
+              :publication_year
+
+  def initialize(book_info)
+    @title = book_info[:title]
+    @author = "#{book_info[:author_first_name]} #{book_info[:author_last_name]}"
+    # args[:author_first_name] + " " + args[:author_last_name]
+    @publication_year = book_info[:publication_date][-4..-1]
+  end
 end
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,9 +1,9 @@
 require './lib/book'
 
 RSpec.describe Book do
-  let(:book) { Book.new(author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960") }
+  let(:book) { Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"}) }
 
-  describe '#intialize' do
+  describe '#initialize' do
     it 'exists' do
      expect(book).to be_a(Book)
     end
@@ -11,7 +11,7 @@ RSpec.describe Book do
     it 'has attributes' do
       expect(book.title).to eq("To Kill a Mockingbird")
       expect(book.author).to eq("Harper Lee")
-      expect(book.piblication_year).to eq("1960")
+      expect(book.publication_year).to eq("1960")
     end
   end
 end
