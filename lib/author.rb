9e4e69f7d7574475ba33c82c4c22f966b72b9e5f
Boston Lowrey <bostonlowrey@gmail.com>
2023-02-24 11:48:11 -0500

    feat: added #name and finished #write

--- lib/author.rb
+++ lib/author.rb
@@ -1,9 +1,27 @@
 class Author
-  attr_reader :name,
+  attr_reader :first_name, 
+              :last_name,
               :books
 
   def initialize(author)
-    @name = author[:first_name] + " " + author[:last_name]
+    @first_name = author[:first_name]
+    @last_name = author[:last_name]
     @books = []
+  end
+  
+  def name
+    "#{first_name} #{last_name}"
+  end
+
+  def write(title, publication_date)
+    book = Book.new({
+      author_first_name: first_name,
+      author_last_name: last_name,
+      title: title,
+      publication_date: publication_date
+    })
+
+    @books << book
+    book
   end
 end
--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe Author do
     
     it 'has attributes' do
       expect(charlotte_bronte.name).to eq("Charlotte Bronte")
+      require 'pry'; binding.pry
       expect(charlotte_bronte.books).to eq([])
     end
   end
@@ -18,12 +19,12 @@ RSpec.describe Author do
   describe '#write' do
   it 'lets author write a book and stores the written books in the books array' do
     jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
-    
+
     expect(jane_eyre).to be_a(Book)
     expect(jane_eyre.title).to eq("Jane Eyre")
     
     villette = charlotte_bronte.write("Villette", "1853")
-    
+   
     expect(charlotte_bronte.books).to eq([jane_eyre, villette])
     end
   end
