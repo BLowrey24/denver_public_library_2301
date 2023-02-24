997d11aabd9046b04a1a0bef398faadd733ca38b
Boston Lowrey <bostonlowrey@gmail.com>
2023-02-24 11:35:26 -0500

    add test: for #write in author

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -14,4 +14,17 @@ RSpec.describe Author do
       expect(charlotte_bronte.books).to eq([])
     end
   end
+  
+  describe '#write' do
+  it 'lets author write a book and stores the written books in the books array' do
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    
+    expect(jane_eyre).to be_a(Book)
+    expect(jane_eyre.title).to eq("Jane Eyre")
+    
+    villette = charlotte_bronte.write("Villette", "1853")
+    
+    expect(charlotte_bronte.books).to eq([jane_eyre, villette])
+    end
+  end
 end
